3e583592f195524cb983be8d53880cac033a4563
ilka2 <kravtsov-ilya@mail.ru>
2024-10-11 06:16:37 +0300

    Добавьте файлы проекта.

---
+++ 7_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class node
+{
+public:
+    int m_value;
+    
+    shared_ptr<node> parent;
+
+    node(int value) : m_value{ value }
+    {
+        if (parent.use_count() == 0)
+        {
+            this-> ~node();
+        }
+    };
+
+    ~node() { cout << "destructor called\n"; }
+};
+
+int main()
+{
+    {
+        auto node1 = make_shared<node>(1);
+        auto node2 = make_shared<node>(2);
+        node1->parent = node2;
+        node2->parent = node1;
+    }
+
+    return 0;
+}
